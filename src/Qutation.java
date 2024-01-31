0b4873a70dd62b99d2ad545d6f4a86f360ed8e69
0Sannya0 <121272072+0Sannya0@users.noreply.github.com>
2024-01-31 17:14:33 +0200

    Update Qutation.java

--- src/Qutation.java
+++ src/Qutation.java
@@ -1,5 +1,5 @@
 public class Quotation {
-    String quote = "\nWelcome to Sun!";
+    String quote = "\nMusic in the soul can be heard by the universe";
     public void display() {
       System.out.println(quote);
     }
